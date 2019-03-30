aa666214958b3a1a7a3fe181b1174e6da72b57d7
Natalia Barbosa <jandreaj100@gmail.com>
2019-03-30 09:03:02 -0500

    pequena cambio en println

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -18,11 +18,11 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  Serial.println("Alto");
-  delay(3000);
+  Serial.println("Prendido");
+  delay(2000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Bajo");
+  Serial.println("Apagado");
    // wait for a second
-  delay(3000);
+  delay(2000);
 }
